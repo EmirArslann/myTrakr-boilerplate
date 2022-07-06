fa355ad2f4d27244fc3e912b3e1f10df237dc777
Emir Furkan Arslan <95889438+EmirArslann@users.noreply.github.com>
2022-07-06 13:36:21 -0700

    setting account

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -1,3 +1,4 @@
+
 class Account {
   constructor(username) {
     this.username = username;
@@ -9,4 +10,63 @@ class Account {
       return total + transaction;
     }, 0);
   }
+  
+  
+ 
+}
+let accounts = []
+
+const getInput = () =>{
+   let inpt = document.querySelector('#input').value
+   console.log(inpt, "inpt")
+   if(inpt === ""){
+    alert("You can't create a account without name")
+    return false
+  }
+  
+  }
+
+const getAccounts = () =>{
+  $.ajax({
+    method: 'get',
+    url: 'http://localhost:3000/accounts',
+    dataType: 'json'
+  }).done((acc) => {
+    accounts = [...acc]
+    console.log('accounts', acc);
+    
+  });
+}
+
+
+
+const postAccounts = () =>{
+  const newAccount={
+    username: $("#input").val(),
+    transactions:[]
+  }
+  $.ajax({      
+    method: 'post',
+    data: JSON.stringify( {
+      newAccount
+    }),
+    url: 'http://localhost:3000/accounts',
+    dataType: 'json',
+    contentType:'application/json'
+    }).done((acc) => {
+      data = acc
+      const account = new Account(acc);
+      console.log('data ajax post', acc);
+      console.log(account.username.username);
+      let inpt = document.querySelector('#input').value
+      if(inpt.value === account.username.username){
+        alert("this username already set")
+        return false
+        
+      }
+      $('#select_account').append(`<option>${account.username.username}</option>`);
+      $('#filter_account').append(`<option>${account.username.username}</option>`);
+      $('#username_summary').append(`<li>${account.username.username}</li>`)
+      
+    })
 }
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,12 @@
 $(() => {
   //Start coding here!
+  getAccounts()
+  $("#add-account").click(function(event) {
+    event.preventDefault();
+    const inputValue = getInput()
+    postAccounts(inputValue)
+  })
+ 
+
+
 });
